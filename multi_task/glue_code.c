06f3c581d8ed97b3479a3accc5020a167d0d5fa5
Felipe Musse <felipe.dienstmann-musse@ensimag.grenoble-inp.fr>
2016-02-05 19:03:57 +0100

    Added modified OSEK kernel for the multi-task controller.

--- multi_task/glue_code.c
+++ multi_task/glue_code.c
@@ -1,29 +1,19 @@
-
 /*
-
-Exemple de code glue pour deux tâches périodiques HARMONIQUES
-AVEC COMMUNICATION
-
+Exemple de code glue pour deux tâches périodiques
 - une supposée rapide et frequente et qui doit s'appeler HighTask
-  qui fait tourner un noeud lustre "high"
 - une supposée lente et moins fréquente et qui doit s'appeler LowTask
-  qui fait tourner un noeud lustre "low"
-- les périodes doivent être spécifiés dans kernel_cfg.h avec les macros :
-     HIGH_PERIOD
-     LOW_PERIOD
-  qui doivent être harmoniques : LOW_PERIOD = n * HIGH_PERIOD
+- les périodes doivent être spécifiées dans "periods.h"
+	HIGH_PERIOD
+	LOW_PERIOD
 */
 
 #include "kernel.h"
-#include "periodes.h"
+#include "periods.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
 
 /**** CODE GLUE, A MODIFIER/ADAPTER */
-/* Ici : inclusion du header Lustre */
 #include <string.h>  /* a cause du strlen plus bas */
-#include "low.h"
-#include "high.h"
 
 /***** OSEK : NE PAS MODIFIER */
 DeclareCounter(SysTimerCnt);
@@ -44,164 +34,106 @@ void user_1ms_isr_type2(void)
 
 /* Initialisation et finalisations OSEK */
 void ecrobot_device_initialize() {
-   /*
-      ICI : on peut mettre du code qui sera appelÃ© automatiquement
-            Ã  l'initialisation de la brique
-      (ne rien mettre si pas nÃ©cessaire)
-   */
+	/*
+	ICI : on peut mettre du code qui sera appelé automatiquement
+	A l'initialisation de la brique
+	(ne rien mettre si pas nécessaire)
+	*/
 }
 
 void ecrobot_device_terminate() {
-   /*
-      ICI : on peut mettre du code qui sera appelÃ© automatiquement
-            Ã  la l'extinction de la brique
-      (ne rien mettre si pas nÃ©cessaire)
-   */
+	/*
+	ICI : on peut mettre du code qui sera appelé automatiquement
+	Ã  la l'extinction de la brique
+	(ne rien mettre si pas nécessaire)
+	*/
 }
 
 /**** CODE GLUE, A MODIFIER/ADAPTER */
-/*
-
-REMARQUE :
-
-On utilise l'option "-ctx-static", donc pas de parametres aux "methodes" Lustre
-
-*/
-
 
 /*------------------------------
 	Déclarations
 --------------------------------
-  On met typiquement ici les déclarations
+On met typiquement ici les déclarations
 de variables partagées, de fonctions
 accessoires etc.
 ------------------------------*/
 
-/*
-	Pour gérer les communications :
-*/
-
-_integer H2L;       // buffer High -> Low
-_integer L2H[2];    // double-buffer Low -> High
-_integer L2Hindex;  // 0 ou 1 : indique dans quelle
-                    // case du double-buffer Low doit écrire
-
-_integer Hcpt;      // compte les "instants" High
-
-//Le ratio des periodes :
-//(le compilateur se charge du calcul statiquement)
-#define PRATIO ((int)(LOW_PERIOD/HIGH_PERIOD))
+int h_cpt;
+int l_cpt;
 
 /*------------------------------
 	Initialisations
 --------------------------------
-  On doit se débrouiller pour que NOS initialisations
+On doit se débrouiller pour que NOS initialisations
 soient effectuées. Comme on ne veut PAS TOUCHER
 une seule ligne au code OSEK, on utilise un kernel_cfg.c
 modifié, où une nouvelle fonction, "usr_init()", est
 appelée à l'initialisation du système.
-Cette fonction doit être définie ici.
+CETTE FONCTION DOIT ÊTRE DÉFINIE
+(vide éventuellement)
 ------------------------------*/
 
-void usr_init(void) {
-	high_init();
-	low_init();
-	//Index de la case où L ecrira la premiere fois
-	L2Hindex = 0;
-	//Il faut initialiser le registre où H lira la premiere fois
-	//c'est-a-dire celui ou L n'ecrit pas !
-	L2H[1] = 0;
-}
-
-/* Procédure de sortie */
-/**
- * Affiche a l'ecran "what var"
- */
+/* utilitaire : affichage d'une variable.
+L'affichage DOIT être réalisé en exclusion
+mutuelle.
+D'où l'utilisation du verrou "lcd"
+fourni par le noyau temps réel (cf. kernel_cfg.h)
+*/
 void show_var(char* what, int line, int var) {
-   GetResource(lcd);
-   display_goto_xy(0, line);
-   display_string(what);
-   display_goto_xy(strlen(what)+1, line);
-   display_int(var, 5);
-   display_update();
-   ReleaseResource(lcd);
+	display_goto_xy(0, line);
+	display_string(what);
+	display_goto_xy(strlen(what)+1, line);
+	display_int(var, 5);
+	display_update();
 }
 
+void usr_init(void) {
+	GetResource(lcd);
+	h_cpt = 0;
+	l_cpt = 0;
 
-void high_O_s(_integer val){
-	show_var("high_O_s", 1, val);
-}
+	// Mettre ici des initialisations éventuelles
+	int cpt = 0;
+	show_var("usr_int",4,0);
 
-void high_O_h2l(_integer val){
-	//Communication H -> L 
-	//si Hcpt == 0, on modifie H2L, on est sur qu'il sera lu
-	//par Low avant le prochain Lupto !
-	show_var("high_O_h2l", 2, val);
-	if (Hcpt == 0) {
-		H2L = val;
+	// Du code sequentiel
+	//show_var("Calibrage Blanc",3,0);
+	while (1) {
+		cpt++;
+		show_var("cpt",5,cpt);
+		if ( ecrobot_is_ENTER_button_pressed()) break;
+		systick_wait_ms(500);
 	}
-	//sinon rien
-}
-
-void low_O_s(_integer val){
-	show_var("low_O_s", 4, val);
-}
-
-void low_O_l2h(_integer val){
-	//Communication L -> H
-	//écrit dans la case du buffer désignée par L2Hindex :
-	L2H[L2Hindex] = val;
-	show_var("low_O_l2h", 5, val);
+	//systick_wait_ms(1000);
+	//show_var("calibrage noir ",4,0);
+	//while (ecrobot_is_ENTER_button_pressed() == 0) { y++; }
+	//systick_wait_ms(1000);
+	ReleaseResource(lcd);
 }
 
 /*------------------------------
-  Definitions des taches 
+  Definitions des taches
 --------------------------------
 Elles doivent :
 - être déclarées par la macro TASK
 - le code doit se terminer par la macro TerminateTask()
 ------------------------------*/
 
-
 TASK(HighTask) {
-	/* Positionnement des entrées */
-	high_I_e( ecrobot_get_touch_sensor(NXT_PORT_S1) );
 
-	/* On lit toujours dans la case opposée à L2Hindex */
-	high_I_l2h(L2H[!L2Hindex]);
+	h_cpt++;
+	show_var("high", 0, h_cpt);
 
-	/* Appel du step */
-	high_step();
-
-	/* OÙ on en est avec la tache Low ? */
-	if (Hcpt  == 0) {
-		/*
-			Low va démarer un cycle juste apres High ...
-		   on "swape"  le double buffer
-		*/
-		L2Hindex = !L2Hindex;
-	}
-	/* et on fait avancer le compteur ... */
-	Hcpt = (Hcpt+1) % PRATIO;
-	
 	/* OSEK : FINALISATION TASK, NE PAS TOUCHER */
 	TerminateTask();
 }
-	
-	
 
 TASK(LowTask) {
 
-	/* Positionnement des entrées */
-	low_I_e( ecrobot_get_touch_sensor(NXT_PORT_S2) );
-
-	// Com H -> L, on lit toujours la valeur courante de H2L
-	low_I_h2l(H2L);
-
-	/* Appel du step */
-	low_step();
+	l_cpt++;
+	show_var("low ", 1, l_cpt);
 
 	/* OSEK : FINALISATION TASK, NE PAS TOUCHER */
 	TerminateTask();
 }
-
